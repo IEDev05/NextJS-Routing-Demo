531e312989257197ec5998d2da42ffa185503e97
KirtiRaval <hp@INFOELEGANT05>
2025-02-13 13:53:16 +0530

    common menu added

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,39 @@
 'use client'
-import styles from "./page.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function Home() {
   const router = useRouter();
-  //make a function for navigation 
-  const navigation = (name)=>{
-    router.push(name);
-  }
-  return (
-    <div >
-      <main >
-        <User name = "HIms">event function and State</User>
-        <br></br>
-        <Link href="/login">Go to login page</Link>
-        <br></br>
-        <Link href="/about">Go to About page</Link>
-        <br></br>
-        <button onClick={()=>router.push("/login")}>Login Using Button</button>
-        <br></br>
-        <button onClick={()=>navigation("/login")}>Login Using Navigation</button>
-        <br></br>
-        <button onClick={()=>alert("its kartik")}>Click me</button>
 
+  // Function for navigation
+  const navigation = (name) => {
+    router.push(name);
+  };
+
+  return (
+    <div>
+      <main className="container text-center mt-4">
+        <User name="DemoName">Event Function and State</User>
+        <br />
+        <button className="btn btn-primary me-2" onClick={() => router.push("/login")}>
+          Login Using Button
+        </button>
+        <button className="btn btn-success me-2" onClick={() => navigation("/login")}>
+          Login Using Navigation
+        </button>
+        <button className="btn btn-warning" onClick={() => alert("It's Kartik")}>
+          Click me
+        </button>
       </main>
-     
     </div>
   );
 }
-const User=(props)=>{ 
+
+const User = (props) => {
   return (
     <div>
-      <h2>its a User {props.name}</h2>
+      <h2>It's a User {props.name}</h2>
     </div>
-  )
-}
+  );
+};
